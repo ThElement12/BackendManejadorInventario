5bd30123ce850c72bedb8bce893d1a3d7046e1fa
waterpolord <robertgarcia341@gmail.com>
2021-07-19 02:50:52 -0400

    cambiando el puerto a 8082

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const app = require('./app');
 require('./database');
 
 async function main(){
+
+    app.set("port", 8082);
+
     await app.listen(app.get('port'), () => {
+        
         console.log(`Server on port ${app.get('port')}`);
     });
 }
