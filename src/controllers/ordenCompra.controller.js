d6a6e17da7bb6da431286632c107566a39c089b3
ThElement12 <45013990+ThElement12@users.noreply.github.com>
2021-07-20 17:33:40 -0400

    Astoa

--- src/controllers/ordenCompra.controller.js
+++ src/controllers/ordenCompra.controller.js
@@ -28,60 +28,29 @@ ordenCtrl.createOrden = async (req, res) => {
    *  {codigoProducto, cantidad}
    * ]
    */
-  const { fecha, articulos } = req.body;
+  var { fecha, articulos, codigoOrden } = req.body;
+  fecha = new Date(fecha.slice(0, 10));
   try {
     ///Busca el producto que falta en el inventario
-    var articulosNecesarios = productosFaltantes(articulos);
-    if(articulosNecesarios.length === 0){
+    var articulosNecesarios = productosFaltantes(articulos, fecha);
+    if (articulosNecesarios.length === 0) {
       res.status(200).json({ mensaje: "No necesita orden" });
     }
-    
-    ///Busca el suplidor que pueda traerlo en la fecha necesaria
-    fecha = new Date(fecha.slice(0, 10));
-    const suplidores = await articulo_suplidor.aggregate([
-      {
-        $match: {
-          codigoArticulo: codigoProducto.toUpperCase()
-        }
-      },
-      {
-        $sort: {
-          codigoArticulo: 1,
-          tiempoEntrega: 1
 
-        }
-      },
-      {
-        $project: {
-          _id: 0
-        }
-      }
-    ])
-    var selectedSupplier = null;
-    var date;
-    for (i = 0; i < suplidores.length; i++) {
-      if (selectedSupplier === null) {
-        date = new Date()
-        date.setDate(date.getDate() + suplidores[i].tiempoEntrega);
-        if (date <= fecha) {
-          selectedSupplier = suplidores[i]
-          selectedSupplier.fechaEntrega = date;
-        }
-      } else {
-        date = new Date()
-        date.setDate(date.getDate() + suplidores[i].tiempoEntrega);
-        if (selectedSupplier.fechaEntrega <= date && date <= fecha) {
-          selectedSupplier = suplidores[i]
-          selectedSupplier.fechaEntrega = date;
-        }
-      }
+    var suplidores = []
+    ///Busca el suplidor que pueda traerlo en la fecha necesaria
+    for(i = 0; i < articulosNecesarios.length; i++){
+      aux = suplidorCercano(articulosNecesarios[i].articulo, fecha)
+      suplidores.push(aux);
     }
-    if (selectedSupplier == null) {
-      date = new Date()
-      date.setDate(date.getDate() + suplidores[0].tiempoEntrega);
-      selectedSupplier = suplidores[0]
-      selectedSupplier.fechaEntrega = date;
-    }
+
+    const newOrder = new Orden({
+        codigoOrdenCompra: codigoOrden,
+        codigo
+    })
+
+
+    
   } catch (err) {
     console.error(err)
     res.status(500).json(err);
@@ -114,8 +83,9 @@ ordenCtrl.updateOrden = (req, res) => res.json({ mensaje: "Orden actualizada" })
 
 ordenCtrl.deleteOrden = (req, res) => res.json({ mensaje: "Orden borrada" });
 
-const productosFaltantes = function(articulos){
+const productosFaltantes = async function (articulos, fecha) {
   var articulosNecesarios = []
+  ///Lo que hay en inventario
   const inventario = await articulo.aggregate([
     {
       $group: {
@@ -135,12 +105,56 @@ const productosFaltantes = function(articulos){
       }
     }
   ]);
+  ///Calculo del consumo diario
+  var consumo = await movimiento.aggregate([
+    {
+      $match: {
+        tipoMovimiento: "SALIDA",
+      }
+    },
+    {
+      $group: {
+        _id: {
+          CA: "$codigoArticulo",
+          fecha: "$fecha"
+        },
+        movimientoDia: {
+          $sum: "$cantidad"
+        }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        codigoArticulo: "$_id.CA",
+        fecha: "$_id.fecha",
+        movimientoDia: "$movimientoDia"
+      }
+    },
+    {
+      $group: {
+        _id: "$codigoArticulo",
+        consumoDiario: {
+          $avg: "$movimientoDia"
+        }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        codigoArticulo: "$_id",
+        consumoDiario: "$consumoDiario"
+      }
+    }
+  ]);
+
+  cantidadDias = Math.round((fecha.getTime() - new Date().getTime())/(1000 * 3600 * 24));
 
   for (i = 0; i < articulos.length; i++) {
     var aux = articulos[i];
     for (j = 0; j < inventario.length; j++) {
       if (aux.codigoProducto === inventario[i].codigoArticulo) {
-        resto = inventario[i].cantidad - aux.cantidad
+        resto = inventario[i].cantidad - aux.cantidad - Math.round((cantidadDias * consumo[i].consumoDiario))
         if (resto < 0) {
           articulosNecesarios.push({
             articulo: aux.codigoProducto,
@@ -153,5 +167,52 @@ const productosFaltantes = function(articulos){
   }
   return articulosNecesarios;
 }
+const suplidorCercano = async function(articulo, fecha){
+  const suplidores = await articulo_suplidor.aggregate([
+    {
+      $match: {
+        codigoArticulo: articulo.toUpperCase()
+      }
+    },
+    {
+      $sort: {
+        codigoArticulo: 1,
+        tiempoEntrega: 1
 
+      }
+    },
+    {
+      $project: {
+        _id: 0
+      }
+    }
+  ])
+  var selectedSupplier = null;
+  var date;
+  for (i = 0; i < suplidores.length; i++) {
+    if (selectedSupplier === null) {
+      date = new Date()
+      date.setDate(date.getDate() + suplidores[i].tiempoEntrega);
+      if (date <= fecha) {
+        selectedSupplier = suplidores[i]
+        selectedSupplier.fechaEntrega = date;
+      }
+    } else {
+      date = new Date()
+      date.setDate(date.getDate() + suplidores[i].tiempoEntrega);
+      if (selectedSupplier.fechaEntrega <= date && date <= fecha) {
+        selectedSupplier = suplidores[i]
+        selectedSupplier.fechaEntrega = date;
+      }
+    }
+  }
+  if (selectedSupplier == null) {
+    date = new Date()
+    date.setDate(date.getDate() + suplidores[0].tiempoEntrega);
+    selectedSupplier = suplidores[0]
+    selectedSupplier.fechaEntrega = date;
+  }
+  return selectedSupplier;
+
+}
 module.exports = ordenCtrl;
